be0abd4208edb7299a405aa6c2f62d70732e709c
Пользователь <ionmax1311@gmail.com>
2021-11-20 21:10:52 +0200

    calculate

--- src/js/calculate.js
+++ src/js/calculate.js
@@ -1,12 +1,29 @@
-if ($(".calculate-1").hasClass("active")) {
-	$(".step-img").attr("src", "img/icon-step-1.svg");
-	$(".step-text").text("Шаг 1/3");
-}
-if ($(".calculate-2").hasClass("active")) {
-	$(".step-img").attr("src", "img/icon-step-2.svg");
-	$(".step-text").text("Шаг 2/3");
-}
-if ($(".calculate-3").hasClass("active")) {
-	$(".step-img").attr("src", "img/icon-step-3.svg");
-	$(".step-text").text("Шаг 3/3");
-}
+$(".calculate__next-1").click(function (e) {
+	e.preventDefault();
+	$(".calculate-1").removeClass("active");
+	$(".calculate-2").addClass("active");
+});
+
+$(".calculate__next-2").click(function (e) {
+	e.preventDefault();
+	$(".calculate-2").removeClass("active");
+	$(".calculate-3").addClass("active");
+});
+
+$(".calculate__prev-3").click(function (e) {
+	e.preventDefault();
+	$(".calculate-3").removeClass("active");
+	$(".calculate-2").addClass("active");
+});
+
+$(".calculate__prev-2").click(function (e) {
+	e.preventDefault();
+	$(".calculate-2").removeClass("active");
+	$(".calculate-1").addClass("active");
+});
+
+$(".btn-type").click(function (e) {
+	e.preventDefault();
+	$(".btn-type").removeClass("active");
+	$(this).addClass("active");
+});
